17e4e750f0b2fb2ef4c3e33da0e6a7e2bbef0bda
Emanuel Gonzalez <gonzalez2vecesz@gmail.com>
2024-09-04 21:09:10 -0300

    Interface distribucion, modificiaciones en dtdistri, controlador de distribucion, manejadordistribucion

--- Pap1/src/main/java/com/mycompany/pap1/logica/ControladorDistribucion.java
+++ Pap1/src/main/java/com/mycompany/pap1/logica/ControladorDistribucion.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.mycompany.pap1.logica;
 import com.mycompany.pap1.datatypes.dtDistribucion;
 import com.mycompany.pap1.datatypes.dtArticulo;
